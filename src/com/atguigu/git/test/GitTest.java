686246762a163bc6fecd4ad16f97f31f8d6e8911
Mylegendary <2530601422@qq.com>
2016-03-19 19:52:51 +0800

    Update GitTest.java
    update for pull！

--- src/com/atguigu/git/test/GitTest.java
+++ src/com/atguigu/git/test/GitTest.java
@@ -4,5 +4,8 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("jiaozhenwei");
+		System.out.println("liliang");
+		System.out.println("yeyuankai");
+		System.out.println("liuyonghui");
 	}
 }
